41e03413993ec58239d23ee7b87782dccf9a6e52
Alexandre <lehcaldasnovas@gmail.com>
2023-10-30 07:00:40 -0300

    searchBar

--- types/index.ts
+++ types/index.ts
@@ -4,5 +4,14 @@ export interface CustomButtonProps {
   title: string;
   containerStyles?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
-  btnType?: 'button' | 'submit'
+  btnType?: "button" | "submit";
+}
+
+export interface CustomFilterProps {
+  title: string;
+}
+
+export interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
 }
